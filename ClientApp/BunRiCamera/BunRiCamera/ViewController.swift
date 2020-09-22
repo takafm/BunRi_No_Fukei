e2535105d89913be304a1ad8eef8de8a19687d95
takafm <taka23.ohyama@gmail.com>
2020-09-23 00:33:21 +0900

    [update] auto capture and add icon.png

--- ClientApp/BunRiCamera/BunRiCamera/ViewController.swift
+++ ClientApp/BunRiCamera/BunRiCamera/ViewController.swift
@@ -17,16 +17,19 @@ class ViewController: UIViewController, AVCaptureVideoDataOutputSampleBufferDele
     @IBOutlet weak var resultLabel: UILabel!
     @IBOutlet weak var confidenceLabel: UILabel!
     @IBOutlet weak var shutterView: UIView!
-
+    
+    var confidenceVal: Float?
     var streamImage: CIImage?
     var captureLayer: AVCaptureVideoPreviewLayer?
+    var session: AVCaptureSession!
     
-    private let CAPTURE_THRESH: Float = 0.9 // スクショを発動するConfidenceの閾値
+    private let CAPTURE_THRESH: Float = 0.98 // スクショを発動するConfidenceの閾値
 
     override func viewDidLoad() {
         super.viewDidLoad()
         cameraView.layer.cornerRadius = 18
         shutterView.layer.cornerRadius = 18
+        self.shutterView.alpha = 0
         setupCamera()
     }
 
@@ -35,7 +38,7 @@ class ViewController: UIViewController, AVCaptureVideoDataOutputSampleBufferDele
     }
 
     func setupCamera() {
-        let session = AVCaptureSession()
+        self.session = AVCaptureSession()
         captureLayer = AVCaptureVideoPreviewLayer(session: session)
         cameraView.layer.addSublayer(captureLayer!)
 
@@ -57,56 +60,12 @@ class ViewController: UIViewController, AVCaptureVideoDataOutputSampleBufferDele
         self.streamImage = CIImage(cvImageBuffer: buffer)
         bunriDetection()
     }
-    @IBAction func butonTupped(_ sender: Any) {
-        self.fireScreenShot()
-    }
     
-//    func faceDetection(_ buffer: CVImageBuffer) {
-//        let request = VNDetectFaceRectanglesRequest { (request, error) in
-//            guard let results = request.results as? [VNFaceObservation] else { return }
-//            if let image = self.ciImage, let result = results.first {
-//                let face = self.getFaceCGImage(image: image, face: result)
-//                if let cg = face {
-//                    self.showPreview(cgImage: cg)
-//                    self.scanImage(cgImage: cg)
-//                }
-//            }
-//        }
-//
-//        let handler = VNImageRequestHandler(cvPixelBuffer: buffer, options: [:])
-//        try? handler.perform([request])
-//    }
-//
-//    func getFaceCGImage(image: CIImage, face: VNFaceObservation) -> CGImage? {
-//        let imageSize = image.extent.size
-//
-//        let box = face.boundingBox.scaledForCropping(to: imageSize)
-//        guard image.extent.contains(box) else {
-//            return nil
-//        }
-//
-//        let size = CGFloat(300.0)
-//
-//        let transform = CGAffineTransform(
-//            scaleX: size / box.size.width,
-//            y: size / box.size.height
-//        )
-//        let faceImage = image.cropped(to: box).transformed(by: transform)
-//
-//        let ctx = CIContext()
-//        guard let cgImage = ctx.createCGImage(faceImage, from: faceImage.extent) else {
-//            assertionFailure()
-//            return nil
-//        }
-//        return cgImage
-//    }
-//
-//    private func showPreview(cgImage: CGImage) {
-//        let uiImage = UIImage(cgImage: cgImage)
-//        DispatchQueue.main.async {
-//            self.faceImageView.image = uiImage
-//        }
-//    }
+    @IBAction func butonTupped(_ sender: Any) {
+        guard let result = self.resultLabel.text else { return }
+        guard let confidence = self.confidenceVal else { return }
+        self.fireScreenShot(compText: "\(result)\n\(confidence * 100) %")
+    }
     
     private func bunriDetection() {
         guard let model = try? VNCoreMLModel(for: BunRiClassifier().model) else { return }
@@ -117,8 +76,11 @@ class ViewController: UIViewController, AVCaptureVideoDataOutputSampleBufferDele
                 DispatchQueue.main.async {
                     self.resultLabel.text = mostConfidentResult.identifier
                     let confidence = mostConfidentResult.confidence
+                    self.confidenceVal = confidence
                     self.confidenceLabel.text = "Confidence: \(confidence * 100) %"
-                    if confidence >= self.CAPTURE_THRESH { self.fireScreenShot() }
+                    if confidence >= self.CAPTURE_THRESH {
+                        self.fireScreenShot(compText: "\(mostConfidentResult.identifier)\n\(confidence * 100) %")
+                    }
                 }
             }
         
@@ -127,41 +89,51 @@ class ViewController: UIViewController, AVCaptureVideoDataOutputSampleBufferDele
         guard ( try? requestHandler.perform([request]) ) != nil else { return }
     }
     
-    private func fireScreenShot() {
+    private func fireScreenShot(compText: String = "") {
         self.shutterView.alpha = 1
-        UIGraphicsBeginImageContextWithOptions(UIScreen.main.bounds.size, false, 0.0)
-        self.view.drawHierarchy(in: self.view.bounds, afterScreenUpdates: true)
-        let image: UIImage = UIGraphicsGetImageFromCurrentImageContext()!
-        UIGraphicsEndImageContext()
-        UIImageWriteToSavedPhotosAlbum(image, nil, nil, nil)
-        self.shutterView.alpha = 0
+        
+        self.session.stopRunning()
+        
+        if let image = self.streamImage {
+            let context = CIContext(options: nil)
+            if let cgimage = context.createCGImage(image, from: image.extent) {
+                if let compImage = self.compositeUIImage(baseCGImage: cgimage, text: compText) {
+                    UIImageWriteToSavedPhotosAlbum(compImage, nil, nil, nil)
+                }
+            }
+        }
+        
+        DispatchQueue.main.asyncAfter(deadline: .now() + 0.1) {
+            self.shutterView.alpha = 0
+        }
+        
+        DispatchQueue.main.asyncAfter(deadline: .now() + 0.5) {
+            self.session.startRunning()
+        }
     }
-
-//    func scanImage(cgImage: CGImage) {
-//        let image = CIImage(cgImage: cgImage)
-//
-//        guard let model = try? VNCoreMLModel(for: BunRiClassifier().model) else { return }
-//        let request = VNCoreMLRequest(model: model) { request, error in
-//            guard let results = request.results as? [VNClassificationObservation] else { return }
-//            guard let mostConfidentResult = results.first else { return }
-//
-//            DispatchQueue.main.async {
-//                self.resultLabel.text = mostConfidentResult.identifier
-//            }
-//        }
-//        let requestHandler = VNImageRequestHandler(ciImage: image, options: [:])
-//        try? requestHandler.perform([request])
-//    }
+    
+    private func compositeUIImage(baseCGImage: CGImage, text: String = "") -> UIImage? {
+        let uiimage = UIImage(cgImage: baseCGImage)
+        let font = UIFont.boldSystemFont(ofSize: 100)
+        
+        UIGraphicsBeginImageContext(uiimage.size)
+        let rect = CGRect(x: 0, y: 0, width: uiimage.size.width, height: uiimage.size.height)
+        uiimage.draw(in: rect)
+                
+        let textRect = CGRect(x:0, y:0, width:1080, height:500)
+        let textStyle = NSMutableParagraphStyle.default.mutableCopy() as! NSMutableParagraphStyle
+        let textFontAttributes = [
+            NSAttributedString.Key.font: font,
+            NSAttributedString.Key.foregroundColor: UIColor.white,
+            NSAttributedString.Key.backgroundColor: UIColor(red: 0, green: 0, blue: 0, alpha: 0.5),
+            NSAttributedString.Key.paragraphStyle: textStyle
+        ]
+        
+        text.draw(in: textRect, withAttributes: textFontAttributes)
+        
+        let newImage = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+        
+        return newImage
+    }
 }
-
-
-//extension CGRect {
-//    func scaledForCropping(to size: CGSize) -> CGRect {
-//        return CGRect(
-//            x: self.origin.x * size.width,
-//            y: self.origin.y * size.height,
-//            width: (self.size.width * size.width),
-//            height: (self.size.height * size.height)
-//        )
-//    }
-//}
